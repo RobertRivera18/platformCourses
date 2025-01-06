0916f6072b2eaffde227dffb77a8e530521b82e1
Recursos Humanos <talentohumano@grupobm.local>
2025-01-06 17:00:58 -0500

    Ckeckout pay Paypal

---
+++ app/Livewire/ManageShoppingCart.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Livewire;
+
+use CodersFree\Shoppingcart\Facades\Cart;
+use Livewire\Component;
+
+class ManageShoppingCart extends Component
+{
+
+    public function remove($rowId)
+    {
+        Cart::instance('shopping');
+        Cart::remove($rowId);
+        $this->dispatch('cart-updated', Cart::content());
+    }
+
+
+
+    public function render()
+    {
+        return view('livewire.manage-shopping-cart');
+    }
+}
